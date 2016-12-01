d3a3dcad34b898ed187a647be11b5586e9446002
laurenmichelepanken <lpanks35@gmail.com>
2016-12-01 18:49:35 -0500

    12/1

--- D12/materials/jquery-city/js/app.js
+++ D12/materials/jquery-city/js/app.js
@@ -1,2 +1,11 @@
-// 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
+// 1. Create .on("click", function() {} ) handlers for each of the thumbnails: #first, #second, #third, #fourth
 // 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
+      
+$('.thumbnails .zoom').click(function(e){
+      e.preventDefault();
+
+      var photo_fullsize =  $(this).find('img').attr('src');
+
+      $('.woocommerce-main-image img').attr('src', photo_fullsize);
+
+    });
