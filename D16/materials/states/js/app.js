41b240c736382e09905e03fc6d0c08a255c9eab5
laurenmichelepanken <lpanks35@gmail.com>
2016-12-12 21:36:18 -0500

    12.12

---
+++ D16/materials/states/js/app.js
@@ -0,0 +1,21 @@
+var ny = {
+  name: "New York",
+  capital: "Albany",
+  governor: "Andrew Cuomo",
+  usSenators: ["Chuck Schumer", "Kirsten Gillibrand"]
+}
+
+var ct = {
+  name: "Connecticut",
+  capital: "Hartford",
+  governor: "Dannel Malloy",
+  usSenators: ["Richard Blumenthal", "Christopher S. Murphy"]
+}
+
+
+var usSenators = [
+  {
+    name: "Dannel",
+    last: "Malloy"
+  },
+ 
